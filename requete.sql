42d1136c91dd24fecafbb8214b41b7d8a1a509b6
Mitia Jessica <mitiah.jessica@gmail.com>
2025-11-16 20:50:14 +0300

    rectify column name

--- requete.sql
+++ requete.sql
@@ -3,26 +3,26 @@ WHERE team_id IS NULL;
 
 SELECT e.id, first_name, last_name FROM employee e 
 LEFT JOIN leave l 
-ON e.id = l.id_employee
+ON e.id = l.employee_id
 WHERE l.id IS NULL;
 
 
 SELECT l.id, l.start_date, l.end_date, e.first_name, e.last_name, t.name
 FROM leave l 
-INNER JOIN employee e ON l.id_employee = e.id
+INNER JOIN employee e ON l.employee_id = e.id
 INNER JOIN team t ON e.team_id = t.id;
 
 SELECT contract_type, COUNT(id) AS number_of_employees
 FROM employee 
 GROUP BY contract_type;
 
-SELECT COUNT(id_employee) FROM employee e
-INNER JOIN leave l on e.id = l.id_employee
+SELECT COUNT(employee_id) FROM employee e
+INNER JOIN leave l on e.id = l.employee_id
 WHERE start_date <= CURRENT_DATE and end_date >= CURRENT_DATE;
 
 SELECT e.id, e.first_name, e.last_name, t.name
 FROM employee e INNER JOIN team t ON t.id = e.team_id
-INNER JOIN leave l on e.id = l.id_employee
+INNER JOIN leave l on e.id = l.employee_id
 WHERE start_date <= CURRENT_DATE and end_date >= CURRENT_DATE;
 
 
